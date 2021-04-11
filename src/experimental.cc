a42c058a35097e632f2ca9e6873b5d8ad3d51e91
davjd <davidbepic@gmail.com>
2021-04-11 18:26:34 -0400

    Minimal changes.

--- src/experimental.cc
+++ src/experimental.cc
@@ -13,64 +13,64 @@
 #ifndef WX_PRECOMP
 #include <wx/wx.h>
 #endif
-// class MyApp : public wxApp
-// {
-// public:
-//     virtual bool OnInit();
-// };
-// class MyFrame : public wxFrame
-// {
-// public:
-//     MyFrame();
-// private:
-//     void OnHello(wxCommandEvent& event);
-//     void OnExit(wxCommandEvent& event);
-//     void OnAbout(wxCommandEvent& event);
-// };
-// enum
-// {
-//     ID_Hello = 1
-// };
-// wxIMPLEMENT_APP(MyApp);
-// bool MyApp::OnInit()
-// {
-//     MyFrame *frame = new MyFrame();
-//     frame->Show(true);
-//     return true;
-// }
-// MyFrame::MyFrame()
-//     : wxFrame(NULL, wxID_ANY, "Hello World")
-// {
-//     wxMenu *menuFile = new wxMenu;
-//     menuFile->Append(ID_Hello, "&Hello...\tCtrl-H",
-//                      "Help string shown in status bar for this menu item");
-//     menuFile->AppendSeparator();
-//     menuFile->Append(wxID_EXIT);
-//     wxMenu *menuHelp = new wxMenu;
-//     menuHelp->Append(wxID_ABOUT);
-//     wxMenuBar *menuBar = new wxMenuBar;
-//     menuBar->Append(menuFile, "&File");
-//     menuBar->Append(menuHelp, "&Help");
-//     SetMenuBar( menuBar );
-//     CreateStatusBar();
-//     SetStatusText("Welcome to wxWidgets!");
-//     Bind(wxEVT_MENU, &MyFrame::OnHello, this, ID_Hello);
-//     Bind(wxEVT_MENU, &MyFrame::OnAbout, this, wxID_ABOUT);
-//     Bind(wxEVT_MENU, &MyFrame::OnExit, this, wxID_EXIT);
-// }
-// void MyFrame::OnExit(wxCommandEvent& event)
-// {
-//     Close(true);
-// }
-// void MyFrame::OnAbout(wxCommandEvent& event)
-// {
-//     wxMessageBox("This is a wxWidgets Hello World example",
-//                  "About Hello World", wxOK | wxICON_INFORMATION);
-// }
-// void MyFrame::OnHello(wxCommandEvent& event)
-// {
-//     wxLogMessage("Hello world from wxWidgets!");
-// }
+class MyApp : public wxApp
+{
+public:
+    virtual bool OnInit();
+};
+class MyFrame : public wxFrame
+{
+public:
+    MyFrame();
+private:
+    void OnHello(wxCommandEvent& event);
+    void OnExit(wxCommandEvent& event);
+    void OnAbout(wxCommandEvent& event);
+};
+enum
+{
+    ID_Hello = 1
+};
+wxIMPLEMENT_APP(MyApp);
+bool MyApp::OnInit()
+{
+    MyFrame *frame = new MyFrame();
+    frame->Show(true);
+    return true;
+}
+MyFrame::MyFrame()
+    : wxFrame(NULL, wxID_ANY, "Hello World")
+{
+    wxMenu *menuFile = new wxMenu;
+    menuFile->Append(ID_Hello, "&Hello...\tCtrl-H",
+                     "Help string shown in status bar for this menu item");
+    menuFile->AppendSeparator();
+    menuFile->Append(wxID_EXIT);
+    wxMenu *menuHelp = new wxMenu;
+    menuHelp->Append(wxID_ABOUT);
+    wxMenuBar *menuBar = new wxMenuBar;
+    menuBar->Append(menuFile, "&File");
+    menuBar->Append(menuHelp, "&Help");
+    SetMenuBar( menuBar );
+    CreateStatusBar();
+    SetStatusText("Welcome to wxWidgets!");
+    Bind(wxEVT_MENU, &MyFrame::OnHello, this, ID_Hello);
+    Bind(wxEVT_MENU, &MyFrame::OnAbout, this, wxID_ABOUT);
+    Bind(wxEVT_MENU, &MyFrame::OnExit, this, wxID_EXIT);
+}
+void MyFrame::OnExit(wxCommandEvent& event)
+{
+    Close(true);
+}
+void MyFrame::OnAbout(wxCommandEvent& event)
+{
+    wxMessageBox("This is a wxWidgets Hello World example",
+                 "About Hello World", wxOK | wxICON_INFORMATION);
+}
+void MyFrame::OnHello(wxCommandEvent& event)
+{
+    wxLogMessage("Hello world from wxWidgets!");
+}
 
 static std::string base64_encode(const std::string &in) {
 
@@ -181,7 +181,7 @@ std::string get_local_date_from_iso(std::string iso_date) {
 int main() {
 
   // may not be needed.
-  const std::string api_key = "65b5f8a1-13e3-484e-99df-d2eb98";
+  const std::string api_key = "";
   std::string url = "https://api.mysportsfeeds.com/v2.1/pull/nba/"
                     "2020-2021-regular/date/20210319/games.json";
   // "https://api.mysportsfeeds.com/v2.1/pull/nba/2020-2021-regular/date/20210319/player_gamelogs.json?player=10134"
